9d06a8678e0d0c247dfb839927562a2974c4d617
pumpkinjeon <wkghd951223@gmail.com>
2025-02-24 15:12:52 +0900

    feat initial commit

---
+++ src/main/java/com/example/boilerplatespringboot/BoilerplateSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.example.boilerplatespringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoilerplateSpringbootApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(BoilerplateSpringbootApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/example/boilerplatespringboot/BoilerplateSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.boilerplatespringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoilerplateSpringbootApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
